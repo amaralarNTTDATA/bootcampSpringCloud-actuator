9042c1c9340afbe2d8c2dabac6a7bda1346d3468
antoniomaral2003 <antoniomaral2003@gmail.com>
2023-05-10 11:25:25 +0200

    commit inicial

---
+++ src/main/java/com/nttdata/bootcamp/bootcampactuator/BootcampActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.bootcampactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampActuatorApplication.class, args);
+	}
+
+}
